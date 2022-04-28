0cc3ad2b1bb703e325ed58bf100640beadbc8454
RominMartignat <rominmart810@gmail.com>
2022-04-28 11:32:57 -0400

    initial commit

---
+++ tp3/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-container>
+      <b-form @submit.prevent="login">
+        <h1>Connexion</h1>
+        <label>Courriel</label>
+        <b-form-input required v-model="email" type="text" />
+        <label>Mot de passe</label>
+        <b-form-input required v-model="password" type="password" />
+        <b-button variant="primary" type="submit" class="mt-4"
+          >Se connecter</b-button
+        >
+      </b-form>
+
+      <div v-if="authServiceError">{{ authServiceError }}</div>
+    </b-container>
+  </div>
+</template>
+<script>
